52b007608a9ab70d8f621e5fc1106c154b9c6f03
Kevin Jauregui <utm21040103@utma.edu.mx>
2023-01-16 12:45:34 -0600

    Avance 1

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,44 @@
+import { AuthService } from './../services/auth.service';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage {
+  constructor(private authSvc: AuthService, private router: Router) {}
+
+  async onLogin(email, password) {
+    try {
+      const user = await this.authSvc.login(email.value, password.value);
+      if (user) {
+        const isVerified = this.authSvc.isEmailVerified(user);
+        this.redirectUser(isVerified);
+      }
+    } catch (error) {
+      console.log('Error->', error);
+    }
+  }
+
+  async onLoginGoogle() {
+    try {
+      const user = await this.authSvc.loginGoogle();
+      if (user) {
+        const isVerified = this.authSvc.isEmailVerified(user);
+        this.redirectUser(isVerified);
+      }
+    } catch (error) {
+      console.log('Error->', error);
+    }
+  }
+
+  private redirectUser(isVerified: boolean): void {
+    if (isVerified) {
+      this.router.navigate(['admin']);
+    } else {
+      this.router.navigate(['verify-email']);
+    }
+  }
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+ firebaseConfig : {
+  apiKey: "AIzaSyD1CkvvgAqwOPWQ1mdDx26RboxE7Bbxau0",
+  authDomain: "doably-3089f.firebaseapp.com",
+  projectId: "doably-3089f",
+  storageBucket: "doably-3089f.appspot.com",
+  messagingSenderId: "293207124084",
+  appId: "1:293207124084:web:5e25f473aaa4544d63c921",
+  measurementId: "G-TTEH99HPSN"
+}
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
