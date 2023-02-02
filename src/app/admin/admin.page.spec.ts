b6f213bd49dc98fdd9c0568eff079dda9187a606
Kevin Jauregui <utm21040103@utma.edu.mx>
2023-02-01 18:11:38 -0600

    Avance 5.1

--- src/app/admin/admin.page.spec.ts
+++ src/app/admin/admin.page.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-
 import { AdminPage } from './admin.page';
+
 
 describe('AdminPage', () => {
   let component: AdminPage;
